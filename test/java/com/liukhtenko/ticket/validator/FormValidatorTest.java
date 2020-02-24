d4605b9c67e5358ee1db2395c6c982be37db6069
VladimirLiukhtsenka <liukhtenko.v@mail.ru>
2020-02-24 20:39:44 +0300

    add tests

---
+++ test/java/com/liukhtenko/ticket/validator/FormValidatorTest.java
@@ -0,0 +1,43 @@
+package java.com.liukhtenko.ticket.validator;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class FormValidatorTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testIsValidString() {
+        System.out.println("test");
+    }
+
+    @Test
+    public void testIsValidNumber() {
+    }
+
+    @Test
+    public void testIsValidPrice() {
+    }
+
+    @Test
+    public void testIsValidDate() {
+    }
+
+    @Test
+    public void testIsPost() {
+    }
+
+    @Test
+    public void testValidateSigUpForm() {
+    }
+}
